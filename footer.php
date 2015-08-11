07bcb5ae9d0287278ab432a19bf67779653667f0
supriyaas <supriya.mk02@gmail.com>
2015-08-11 21:38:25 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+</div>
+<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
+				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date('Y'); ?> Pretty in Pink</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>

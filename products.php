b76c25dc0f0628dfd587a5d847bb59f6863dbfea
supriyaas <supriya.mk02@gmail.com>
2015-08-11 21:43:31 +0530

    Create products.php

---
+++ products.php
@@ -0,0 +1,64 @@
+<?php
+$products = array();
+
+$products[101] = array(
+    "name" => "Black Top",
+    "img" => "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=44107585",
+    "price" => 18 , 
+    "paypal" => "JQNCMKK8MLL64"
+);
+$products[102] = array(
+    "name" => "Beige Top",
+    "img" => "http://2.imimg.com/data2/UO/KF/MY-1796519/printed-top-250x250.jpg",
+    "price" => 20,
+    "paypal" => "W3KA658NDUEF6"
+);
+$products[103] = array(
+    "name" => "Blue lace Top",
+    "img" => "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=90543191",  
+    "price" => 20,
+    "paypal" =>"VWM4E8ZE9GQ3E"
+    
+);
+$products[104] = array(
+    "name" => "Red Bow Top",
+    "img" => "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=88495023",    
+    "price" => 18,
+    "paypal" =>"GZTUJP8N3P2Z4"
+    
+);
+$products[105] = array(
+    "name" => "Mickey mouse Top",
+    "img" => "http://www.jukupop.com/images/mighty-fine-ladies-mickey-mouse-crop-top-grey-p1230-2669_zoom.jpg",    
+    "price" => 25,
+    "paypal" =>"W3ECDPK4UMCBW" 
+    
+);
+$products[106] = array(
+    "name" => "Denim Top",
+    "img" => "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=100894262",    
+    "price" => 20,
+    "paypal" =>"6XN7XUP6WQ7DJ"
+    
+);
+$products[107] = array(
+    "name" => "Flower red Top",
+    "img" => "http://img.tradeindia.com/fp/1/001/248/146.jpg",    
+    "price" => 20,
+    "payapl" =>"PJVCB4PZFY4W8"
+    
+);
+$products[108] = array(
+    "name" => "Red and Black dress",
+    "img" => "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=68075084",    
+    "price" => 25,
+    
+    "paypal" =>"PKTR7E4BP362J"
+);
+
+
+?>
+
+
+
+

2d572d5ff75a01a4c359315a826e084ce6f7ebe9
supriyaas <supriya.mk02@gmail.com>
2015-08-11 21:37:42 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+	<title><?php echo $pageTitle; ?></title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+	<link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+
+	 <style>
+   .image-container {
+       
+    display: inline-block;
+    position: relative;
+}
+
+.star-button {
+    position: absolute;    
+    right: 60px;
+    top: 1px;
+    height: 70px;
+    width:150px;
+    
+    font-size: 22pt;
+    
+}
+         .conbut
+         {
+            position: absolute;    
+    right: 60px;
+    top: 50px;
+    height: 70px;
+    width:150px;  
+       font-size:22pt;
+             
+         }
+         .shopbut
+         {
+             
+          position: absolute;    
+    right: 60px;
+    top: 100px;
+    height: 70px;
+    width:150px;  
+       font-size:22pt;
+                 
+             
+         }
+         .hombut
+         {
+             
+          position: absolute;    
+    left: 40px;
+    top: 30px;
+    height: 10px;
+    width:10px;  
+       
+         }
+         
+        </style>
+
+<div class="image-container">
+    
+   <img  style="width:1450px;height:200px;" src="http://1.bp.blogspot.com/-5V3CI6f_hGY/UbpRpuRSkAI/AAAAAAAAOzA/nsMVuvY8cSY/s1600/pretty%2Bin%2Bpink-header%25233.png" >
+        
+   <ul  class="nav">
+       <p class="hombut"><a href="index.php"><img style="width:130px;" src="http://www.tcb.com.mx/img/iconos/home.png"></a></p>
+       <p class="star-button" > <a href="shirts.php"><span style="color:white;">Tops</span></a></p>
+       <p class="conbut"><a style="color:white;" href="contact.php">Contact</a></p>
+       
+       <p class="shopbut"><a style="color:white;" target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=LB5DYMZU4KGVQ&amp;display=1">Cart</a></p>
+    </ul>
+
+        </div>
+        
+	<div id="content">

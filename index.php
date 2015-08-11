d2a18e4808211e6ca29a301c33e726281a1165d5
supriyaas <supriya.mk02@gmail.com>
2015-08-11 21:36:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+$pageTitle = "Unique Tshirt For Mike";
+include('inc/header.php'); ?>
+		
+            
+
+			<div class="wrapper">
+
+				
+				<img src="http://www.sulakshana.net/assets/services/individual/personal_shopping/personal_shopping_banner.jpg">
+			</div>
+
+		</div>
+
+		<div class="section shirts latest">
+
+			<div class="wrapper">
+
+				<h2>New Collections</h2>
+
+                
+                
+                
+				<ul class="products">
+                    
+                    
+                    
+					<li><a href="http://localhost/shirtd.php?id=102">
+							<img src="http://2.imimg.com/data2/UO/KF/MY-1796519/printed-top-250x250.jpg">
+							<p>View Details</p>
+						</a>
+					</li><li>
+						<a href="http://localhost/shirtd.php?id=101">
+							<img src="http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=44107585">
+							<p>View Details</p>
+						</a>
+					</li><li>
+						<a href="http://localhost/shirtd.php?id=106">
+							<img src="http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=100894262.jpg">
+							<p>View Details</p>
+						</a>
+					</li><li>
+						<a href="http://localhost/shirtd.php?id=105">
+							<img src="http://www.jukupop.com/images/mighty-fine-ladies-mickey-mouse-crop-top-grey-p1230-2669_zoom.jpg">
+							<p>View Details</p>
+						</a>
+					</li>
+                    
+				</ul>
+
+			</div>
+
+		</div>
+
+
+	<?php include('inc/footer.php'); ?>
